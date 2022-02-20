cfe4f394d0dae0d0c780d72985ebfb1f89116858
Kai Wang <kdwang0917@gmail.com>
2022-02-19 22:28:31 -0800

    implement discord q&a functionality

--- discord_bot.py
+++ discord_bot.py
@@ -1,10 +1,12 @@
-import random, requests, sys, discord
+import random, requests, asyncio, discord, time
 
 # TODO
-# - implement answer detection (use same message detection function or make new one or implement into a def function)
-# - timer for answers
+# - fix json parsing issue
 # - permanent stats for users
+# - add answer streak feature
 # - find a server to run the bot in the cloud 24/7?
+# - add comments and make code prettier (get rid of excess variables, make code more organized)
+# - implement 'settings' feature
 
 token = open('token.txt', 'r')
 TOKEN = token.read()
@@ -19,30 +21,98 @@ qtype = 0
 answering_state = False
 counter = 0
 user_list = []
+question = ''
+answers_dic = {}
+players_dic = {}
+correct_players = []
 
 def update_url():
     global url
     url = 'https://opentdb.com/api.php?amount=' + str(amount) + '&category=' + str(category) + '&difficulty=' + str(difficulty) + '&type=' + str(qtype)
-        
+
+async def print_question(message):
+    global answering_state, url, counter, question, correct_players
+
+    update_url()
+
+    response = requests.get(url)
+    trivia = response.json()
+    results = trivia['results']
+
+    for x in range(int(amount)):
+        if (answering_state==True):
+
+            question = results[x]
+
+            await message.channel.send(f'''
+----------------------------------------
+{question['question']}
+''')
+
+            if (question['type'] == 'multiple'):
+                answers = question['incorrect_answers']
+                answers.append(question['correct_answer'])
+                random.shuffle(answers)
+                answers_dic = {
+                    'A': answers[0],
+                    'B': answers[1],
+                    'C': answers[2],
+                    'D': answers[3]
+                    }
+
+                await message.channel.send(f'''
+A: {answers[0]}
+B: {answers[1]}
+C: {answers[2]}
+D: {answers[3]}
+''')
+                await asyncio.sleep(10)
+                for x in players_dic:
+                    if ((players_dic[x].capitalize() in answers_dic)and(answers_dic[players_dic[x].capitalize()]==question['correct_answer'])):
+                        correct_players.append(x)
+                await message.channel.send('The answer is: '+question['correct_answer'])
+                await message.channel.send(f'''
+Congratulations: 
+{correct_players}
+''')
+            elif(question['type'] == 'boolean'):
+                await message.channel.send('True OR False?')
+                await asyncio.sleep(10)
+                for x in players_dic:
+                    if (players_dic[x].capitalize()==question['correct_answer']):
+                        correct_players.append(x)
+                await message.channel.send('The answer is: '+question['correct_answer'])
+                await message.channel.send(f'''
+Congratulations: 
+{correct_players}
+''')
+            correct_players.clear()
+            players_dic.clear()
+            await asyncio.sleep(3)
+    await message.channel.send('Game over! Thanks for playing.')
+    answering_state = False
+
+
 @client.event
 async def on_ready():
     print('Logged in with {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-    global counter
+    global counter, answering_state, question, answers_dic, players_dic
 
+    #Keep track of all the messages in the bot channel
     username = str(message.author).split('#')[0]
     user_message = str(message.content)
     print(f'{username}: {user_message}')
 
-    if ((username in user_list)==False):
-        user_list.append(username)
-        if ('TriviaBot' in user_list):
-            user_list.remove('TriviaBot')
-
+    #Make sure bot does not interact with itself
     if message.author == client.user:
         return
+
+    #Add new users to user list & make sure the bot is not in the list
+    if ((username in user_list)==False):
+        user_list.append(username)
 
     match user_message:
         case '$tryhard check':
@@ -57,50 +127,28 @@ async def on_message(message):
                     await message.channel.send(user + " is literally the sweatiest tryhard in the universe")
                 case 3:
                     await message.channel.send(user + " never tryhards")
-            return
         case '$help':
             await message.channel.send('''
 This is a Trivia bot made by Kai Wang using the OpenTrivia database. Available commands:
     $start -> Begin playing
     $settings -> Change the game settings
-    $quit -> Stop the current game
-    $tryhard check -> ;)
+    $stop -> Stop the current game
+    $tryhard check -> :)
 ''')
-            return
         case '$start':
-            update_url()
+            if (answering_state == False):
+                answering_state = True
+                await print_question(message)
+            elif (answering_state == True):
+                await message.channel.send("Existing game is already running. Enter $stop to terminate.")
+        case '$stop':
+            answering_state = False
+            await message.channel.send("Game terminated")
+        case _:
+            if (answering_state == True):
+                players_dic[username] = user_message
+                
 
-            response = requests.get(url)
-            trivia = response.json()
-            results = trivia['results']
-
-            if (counter < amount):
-
-                question = results[counter]
-
-                await message.channel.send(question['question'])
-
-                if (question['type'] == 'multiple'):
-                    answers = question['incorrect_answers']
-                    answers.append(question['correct_answer'])
-                    random.shuffle(answers)
-                    answers_dic = {
-                        'A': answers[0],
-                        'B': answers[1],
-                        'C': answers[2],
-                        'D': answers[3]
-                        }
-
-                    await message.channel.send(f'''
-A: {answers[0]}
-B: {answers[1]}
-C: {answers[2]}
-D: {answers[3]}
-''')
-                elif(question['type'] == 'boolean'):
-                    await message.channel.send('True OR False?')
-
-                counter += 1
     
 if __name__ == '__main__':
     client.run(TOKEN)
